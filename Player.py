b012b48710ed56b23ac3fdec3a0a17e8536f49d8
aidanmonfort <aidanmonfort1271@gmail.com>
2023-02-08 11:35:59 -0800

    Closer

---
+++ Player.py
@@ -0,0 +1,94 @@
+import pygame
+from Map import *
+
+class Player:
+    def __init__(self, color=(255, 255, 255)):
+        self.x = 400
+        self.y = 400
+        self.dY = 0
+        self.dX = 0
+        self.width = 30
+        self.height = 30
+        self.color = color
+        self.isJump = False
+        self.score = 0
+
+    def setMap(self, map):
+        self.map = map
+
+    def step(self):
+        self.dY -= self.map.getGrav()
+        if self.dY > 10:
+            self.dY = 10
+        self.dX *= .75
+        self.x += self.dX
+        self.y += self.dY
+        if self.is_map_bottom_collision():
+            self.y -= self.dY
+            self.isJump = False
+        if self.is_map_right_collision():
+            self.x -= self.dX
+
+        self.checkCoinCollisions()
+
+        return self.x, self.y
+
+    def jump(self):
+        if not self.isJump:
+            self.dY = -15
+            self.isJump = True
+
+    def checkCoinCollisions(self):
+        for c in self.map.coins:
+            if c.getCollisionRect().colliderect(self.getCollisionRect()):
+                self.map.coins.remove(c)
+                self.score += 1
+
+    def stepRight(self):
+        self.dX = 10
+
+    def stepLeft(self):
+        self.dX = -10
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.getCollisionRect())
+
+    def getCollisionRect(self):
+        return pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def is_map_collision(self):
+        myHitBox = pygame.Rect(self.x, self.y, self.width, self.height)
+        mapHitBoxes = self.map.get_hit_box_list()
+        for box in mapHitBoxes:
+            if myHitBox.colliderect(box):
+                return True
+
+        return False
+
+    def is_map_bottom_collision(self):
+        myHitBox = pygame.Rect(self.x, self.y, self.width, self.height)
+        myBottomY = myHitBox.y + myHitBox.height
+        mapHitBoxes = self.map.get_hit_box_list()
+        for box in mapHitBoxes:
+            if myHitBox.colliderect(box):
+                if myBottomY > box.y: # I am hitting and BELOW this platform
+                    return True
+
+        return False
+
+
+    def is_map_right_collision(self):
+        myHitBox = pygame.Rect(self.x, self.y, self.width, self.height)
+        myRightX = myHitBox.x + myHitBox.width
+        mapHitBoxes = self.map.get_hit_box_list()
+        for box in mapHitBoxes:
+            if myHitBox.colliderect(box):
+                if myRightX > box.x: # I am hitting and to the RIGHT this platform
+                    return True
+
+        return False
+
+    def get_score(self):
+        return self.score
+
+
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import random
 import pygame
-from classes import *
+from Player import *
+from DnaStrand import *
+from Coins import *
 
 
 #start the pygame engine
@@ -15,26 +17,65 @@ pygame.mixer.init()
 
 #game variables
 simOver = False
-p1 = Player()
+p1 = PlayerAI()
+players = []
 mapOne = Map(-.5)
 
 #game independent variables (needed for every pygame)
 FPS = 60 #60 Frames Per Second for the game update cycle
 fpsClock = pygame.time.Clock() #used to lock the game to 60 FPS
 screen = pygame.display.set_mode((1280, 720)) #initialize the game window
+camera_pos = (0, 0)
+world = pygame.Surface((3000,3000))
+
+def createPlayerAI():
+    for i in range(50):
+        players.append(PlayerAI())
+
 
 def createMapOne():
-    mapOne.add(Platform(120, 600, 300, 10, (0, 255, 0)))
+    mapOne.add(Platform(0, 690, 2400, 30, (0, 255, 0)))
+    mapOne.add(Platform(100, 600, 400, 30, (0, 255, 0)))
+    mapOne.add(Platform(200, 500, 400, 30, (0, 255, 0)))
+    mapOne.add(Platform(100, 350, 200, 30, (0, 255, 0)))
+    mapOne.add(Platform(270, 250, 200, 30, (0, 255, 0)))
+    mapOne.add(Platform(600, 200, 30, 400, (0, 255, 0)))
+    mapOne.add(Platform(715, 120, 300, 30, (0, 255, 0)))
+    mapOne.add(Platform(740, 700, 300, 30, (0, 255, 0)))
+    mapOne.add(Coin(600, 650))
+    mapOne.add(Coin(220, 450))
+    # mapOne.add(Coin())
+
+
+def draw_mouse_coords():
+    textSurface = myFont.render(str(pygame.mouse.get_pos()), True, (255,255,255))
+    world.blit(textSurface, (50, 30))
 
 def clear_screen():
-    pygame.draw.rect(screen, (0, 0, 0), (0, 0, 1280, 720))
+    pygame.draw.rect(world, (0, 0, 0), (0, 0, pygame.Surface.get_height(world), pygame.Surface.get_width(world)))
 
 def input():
-    if pygame.key.get_pressed()[pygame.K_SPACE]:
-        p1.jump()
+    global camera_pos
+    if pygame.key.get_pressed()[pygame.K_d]:
+        camera_pos = (camera_pos[0] - 10, camera_pos[1])
+    if pygame.key.get_pressed()[pygame.K_a]:
+        camera_pos = (camera_pos[0] + 10, camera_pos[1])
+    if pygame.key.get_pressed()[pygame.K_w]:
+        camera_pos = (camera_pos[0], camera_pos[1] + 10)
+    if pygame.key.get_pressed()[pygame.K_s]:
+        camera_pos = (camera_pos[0], camera_pos[1] - 10)
+    if camera_pos[0] < 0:
+        camera_pos = (0, camera_pos[1])
+    if camera_pos[1] < 0:
+        camera_pos = (camera_pos[0], 0)
+
+
 
 createMapOne()
-p1.setMap(mapOne)
+createPlayerAI()
+for p in players:
+    p.setMap(mapOne)
+
 #main while loop
 while not simOver:
     clear_screen()
@@ -45,11 +86,20 @@ while not simOver:
         if event.type == pygame.QUIT:
            simOver = True
 
+    for p in players:
+        p.draw(world)
+    mapOne.draw(world)
     input()
-    p1.draw(screen)
-    p1.step()
-    mapOne.draw(screen)
+    draw_mouse_coords()
+
+    # player update code
+    for p in players:
+        p.act()
+
+
+
     #put all the graphics on the screen
     #should be the LAST LINE of game code
+    screen.blit(world, camera_pos)
     pygame.display.flip()
     fpsClock.tick(FPS) #slow the loop down to 60 loops per second
